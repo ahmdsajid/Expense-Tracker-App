e496f2a6f7a7aa23372a59f5ac3fdd82b1a440fd
Mahmudul Sajid <155292895+ahmdsajid@users.noreply.github.com>
2024-11-07 00:26:37 +0600

    update

--- expanse_tracker/lib/Model/expensedata.dart
+++ expanse_tracker/lib/Model/expensedata.dart
@@ -1,13 +1,16 @@
-
 import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
-class Expensedata{
-  Expensedata({required this.title, required this.amount, required this.date}): id =uuid.v4();
+enum Category { food, work, traval, leisure }
+
+class Expensedata {
+  Expensedata({required this.title, required this.amount, required this.date, required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
